36ccbd0480cad60db1d670a6c721e1fa18b04d10
mazumdaratik <rjatik990@gmail.com>
2025-06-18 00:45:02 +0600

    Home component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import About from './components/about/About'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
-import Skills from './components/skills/Skills'
 
 function App() {
-
 
   return (
     <>
       <Header />
+      <main className='main'>
+        <Home />
+      </main>
     </>
   )
 }
