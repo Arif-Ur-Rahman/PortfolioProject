2466f5ab6b8379ab852e20b29d5bff085408d97d
Atik Mazumdar <rjatik990@gmail.com>
2025-07-01 00:10:59 +0600

    added animation to navbar items

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,18 @@ import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
+      window.addEventListener("scroll", function () {
+        const header = this.document.querySelector(".header");
+        if(this.scrollY >= 80) header.classList.add("scroll-header");
+        else header.classList.remove("scroll-header");
+    })
+
   const [Toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState("#home");
 
   return (
     <header className="header">
-      <nav className="nav container">
+      <nav className="nav navbar container">
         <a href="" className="nav__logo">
           <img src="./src/assets/atik.png" alt="logo" />
         </a>
@@ -14,7 +21,7 @@ const Header = () => {
         <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list grid">
             <li className="nav__item">
-              <a href="#home" className="nav__link active-link">
+              <a href="#home" onClick={ () => setActiveNav("#home")} className={activeNav === "#home" ? "nav__link active-link": "nav__link"}>
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
