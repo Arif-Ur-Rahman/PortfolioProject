aacfc168403073c700c67608bd6727c2e8f44450
Atik Mazumdar <rjatik990@gmail.com>
2025-07-01 00:23:14 +0600

    side scroll animation added to about component

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
+import AOS from 'aos'; 
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
 
 const Info = () => {
+
+    useEffect(() => {
+    AOS.init({
+      duration: 800,
+      once: true,
+    });
+
+    AOS.refresh()
+  }, []);
   return (
     <div className="about__info grid">
-        <div className="about__box">
+        <div className="about__box" data-aos="fade-up">
             <i className="bx bx-award about__icon"></i>
             <h3 className="about__title">Experience</h3>
             <span className="about__subtitle">1 years working</span>
         </div>
 
-        <div className="about__box">
+        <div className="about__box" data-aos="zoom-in" >
             <i className="bx bx-briefcase-alt about__icon"></i>
             <h3 className="about__title">Completed</h3>
             <span className="about__subtitle">30+ projects</span>
         </div>
 
-        <div className="about__box">
+        <div className="about__box" data-aos="fade-up">
             <i className="bx bx-support about__icon"></i>
             <h3 className="about__title">Support</h3>
             <span className="about__subtitle">24/7 Online</span>
